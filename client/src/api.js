e4d331829720a58c2c40df84a918f9aa105ebd52
Amaan Ansari <ansari028amaan@gmail.com>
2024-04-16 22:13:18 +0530

    change timeout

--- client/src/api.js
+++ client/src/api.js
@@ -4,7 +4,6 @@ import { logout } from "./shared/utils/auth";
 const apiClient = axios.create({
   // baseURL: "http://localhost:5002/api/v1",
   baseURL: "https://videochatapp-b5bx.onrender.com/api/v1",
-  timeout: 1000,
 });
 apiClient.interceptors.request.use(
   (config) => {
